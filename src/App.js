f3e131c3760e83f319c2eb37c4a4e21aabf736e8
Madeline Prickette <madelineprickette@gmail.com>
2022-05-03 11:53:34 -0500

    did code along with Edan and class

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  // useSelector grabs data from the global redux store
+  // Grab the likeCount from our redux store
+
+let likeCount = useSelector(store => store); 
+  // Because you are not asking for the whole store
+
+  //use the dispatch
+  let dispatch = useDispatch();
+
+  //REDUCER
+  // Whatever is returned from this function
+  // is our global state aka uncle aka nevada
+const onLikeMe = () => { // This is called a "dispatch"
+  // Tell my store that I was liked
+  dispatch({
+    
+    type: "SOMEONE_LIKES_ME" // Yelling to another file
+  })
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <>
+        <h1>
+          I am loved
+        </h1>
+
+        <button onClick={onLikeMe}>
+          Like me
+          </button>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          {likeCount} likes
+          </p>
+      </>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'; //grabbing one thing from the redux library
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Whateve is returned from this function 
+  // is our global state (aka "store")
+
+  // Stimilar to:
+  // let [likeCount, setLikeCount] = useState(5732)
+const likeCount = (state = 5732, action) => {
+  console.log('state is', state);
+  console.log('action is', action);
+
+  if (action.type === "SOMEONE_LIKES_ME"){
+    return state + 1;
+  }
+
+  return state;
+};
+
+// Create out resux store aka Nevada aka the cool uncle
+const storeInstance = createStore(likeCount);
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Where we first render the app.
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Provider shares out redux store with all of our components*/}
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
